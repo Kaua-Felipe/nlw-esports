2b6c8e7d7460675b768149d30838d45b567431ea
Kauã <kauafelipeoficial191@gmail.com>
2022-09-14 10:53:34 -0300

    Iniciando a parte do Server do projeto

---
+++ server/src/server.ts
@@ -0,0 +1,33 @@
+import express from "express"
+
+const app = express()
+
+app.get("/ads", (req, res) => {
+    return res.json([
+        {
+            id: 1, 
+            name: "Anúncio 1"
+        }, 
+        {
+            id: 2, 
+            name: "Anúncio 2"
+        }, 
+        {
+            id: 3, 
+            name: "Anúncio 3"
+        }, 
+        {
+            id: 4, 
+            name: "Anúncio 4"
+        }, 
+        {
+            id: 5, 
+            name: "Anúncio 5"
+        }, 
+        {
+            id: 6, 
+            name: "Anúncio 6"
+        }
+    ])
+})
+app.listen(3333, () => console.log("Server Runing..."))
